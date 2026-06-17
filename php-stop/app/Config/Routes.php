8bd19959cb419121591837fa0cdfc0ad17e83a04
Ray <raymond4114p@gmail.com>
2026-06-17 11:27:40 +0700

    feat(stop): add stop service routes for stops, passenger count, alerts, and health

--- php-stop/app/Config/Routes.php
+++ php-stop/app/Config/Routes.php
@@ -2,5 +2,14 @@
 
 use CodeIgniter\Router\RouteCollection;
 
-/** @var RouteCollection $routes */
+/**
+ * @var RouteCollection $routes
+ */
 $routes->get('/', 'Home::index');
+
+// Stop Service Routes
+$routes->get('api/stops', 'StopController::index');
+$routes->get('api/stops/(:num)/status', 'StopController::status/$1');
+$routes->post('api/passengers/count', 'CountController::store');
+$routes->get('api/alerts', 'AlertController::index');
+$routes->get('health', 'HealthController::index');
