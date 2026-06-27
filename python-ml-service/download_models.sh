7ad12beec957b75f004a7debab93557858ca3e31
wooffyy <wahfiuddin3173@gmail.com>
2026-06-27 23:07:41 +0700

    feat(ml): update model 1 service to use new prediction logic and download updated models

--- python-ml-service/download_models.sh
+++ python-ml-service/download_models.sh
@@ -6,9 +6,9 @@ mkdir -p $MODEL_DIR
 
 echo "Downloading ML models..."
 
-gdown "1fXjUHRt4nMDVvPN-7txPDZDs4nsbe9SU" -O $MODEL_DIR/model1_delay_regressor_v2.pkl
-gdown "11Tim9EpymUctMMPhy4eo8oNvjhg2Rmeo" -O $MODEL_DIR/model1_corridor_encoder_v2.pkl
-gdown "1pFxeFEElNTVNeyf2ewRnqSQ1zkXZexkE" -O $MODEL_DIR/model1_config_v2.json
+gdown "1dRXjoN5l-Y2fDwebfiufapjKWf1kEGIc" -O $MODEL_DIR/model1_delay_regressor_v2.pkl
+gdown "1qSv2J2FHCsS-QAc_bk6pqq-6AGnKvEzw" -O $MODEL_DIR/model1_corridor_encoder_v2.pkl
+gdown "1600LWfZ_58qJkNnE66pHaXbmI4QcwiIN" -O $MODEL_DIR/model1_config_v2.json
 gdown "1a7ygkYeJsJ03yGpUH_VT-IeHkWP3RC25" -O $MODEL_DIR/model2_crowd_classifier.pkl
 gdown "1VOy0EYhTNMN1X5gTBRhdIWJihm2TvmMa" -O $MODEL_DIR/model2_target_mapping.pkl
 gdown "1kT8ApbumnX3sBllPLjpJJzpH5vbhnMkY" -O $MODEL_DIR/model2_feature_order.pkl
